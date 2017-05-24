b9b8acbc470017185f310a2ced1955a1c67fe726
tungqddev <quachdaitung@gmail.com>
2017-05-24 17:55:30 +0700

    multi db runner

---
+++ MultiDatabaseExecuter/FormDatabaseExecuter.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MultiDatabaseExecuter
+{
+    partial class frmDatabaseExecuter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDatabaseExecuter));
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblServerName = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnImportQuery = new System.Windows.Forms.Button();
+            this.rchtxtLogs = new System.Windows.Forms.RichTextBox();
+            this.chklsbxDBList = new System.Windows.Forms.CheckedListBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(99, 24);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(228, 20);
+            this.txtServerName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(99, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(228, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(99, 50);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(228, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // lblServerName
+            // 
+            this.lblServerName.AutoSize = true;
+            this.lblServerName.Location = new System.Drawing.Point(22, 31);
+            this.lblServerName.Name = "lblServerName";
+            this.lblServerName.Size = new System.Drawing.Size(67, 13);
+            this.lblServerName.TabIndex = 3;
+            this.lblServerName.Text = "Sever name:";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(22, 57);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(36, 13);
+            this.lblLogin.TabIndex = 4;
+            this.lblLogin.Text = "Login:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 83);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(351, 21);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(105, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(474, 21);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(121, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnImportQuery
+            // 
+            this.btnImportQuery.Location = new System.Drawing.Point(351, 57);
+            this.btnImportQuery.Name = "btnImportQuery";
+            this.btnImportQuery.Size = new System.Drawing.Size(105, 39);
+            this.btnImportQuery.TabIndex = 8;
+            this.btnImportQuery.Text = "IMPORT QUERY";
+            this.btnImportQuery.UseVisualStyleBackColor = true;
+            this.btnImportQuery.Click += new System.EventHandler(this.btnImportQuery_Click);
+            // 
+            // rchtxtLogs
+            // 
+            this.rchtxtLogs.Location = new System.Drawing.Point(25, 128);
+            this.rchtxtLogs.Name = "rchtxtLogs";
+            this.rchtxtLogs.Size = new System.Drawing.Size(302, 215);
+            this.rchtxtLogs.TabIndex = 9;
+            this.rchtxtLogs.Text = "";
+            // 
+            // chklsbxDBList
+            // 
+            this.chklsbxDBList.FormattingEnabled = true;
+            this.chklsbxDBList.Location = new System.Drawing.Point(351, 129);
+            this.chklsbxDBList.Name = "chklsbxDBList";
+            this.chklsbxDBList.Size = new System.Drawing.Size(244, 214);
+            this.chklsbxDBList.TabIndex = 11;
+            this.chklsbxDBList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chklsbxDBList_KeyDown);
+            this.chklsbxDBList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.chklsbxDBList_KeyPress);
+            this.chklsbxDBList.KeyUp += new System.Windows.Forms.KeyEventHandler(this.chklsbxDBList_KeyUp);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.ForeColor = System.Drawing.Color.Red;
+            this.btnExecute.Location = new System.Drawing.Point(474, 57);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(121, 42);
+            this.btnExecute.TabIndex = 12;
+            this.btnExecute.Text = "EXECUTE";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // frmDatabaseExecuter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 390);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.chklsbxDBList);
+            this.Controls.Add(this.rchtxtLogs);
+            this.Controls.Add(this.btnImportQuery);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblServerName);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtServerName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDatabaseExecuter";
+            this.Text = "Database Executer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblServerName;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnImportQuery;
+        private System.Windows.Forms.RichTextBox rchtxtLogs;
+        private System.Windows.Forms.CheckedListBox chklsbxDBList;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
+
---
+++ MultiDatabaseExecuter/FormDatabaseExecuter.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace MultiDatabaseExecuter
+{
+    public partial class frmDatabaseExecuter : Form
+    {
+        public frmDatabaseExecuter()
+        {
+            InitializeComponent();
+        }
+
+        private void connectDatabase()
+        {
+
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=" + txtServerName.Text.ToString() + ";Initial Catalog=master;User ID=" + txtLogin.Text.ToString() + ";Password=" + txtPassword.Text.ToString();
+            cnn = new SqlConnection(connetionString);
+            SqlCommand cmdGetDataBaseList = new SqlCommand();
+            SqlDataReader reader;
+            DataTable tblMyDatabaseList = new DataTable();
+            try
+            {
+                cnn.Open();
+                cmdGetDataBaseList.CommandText = "select * from master.sys.databases";
+                cmdGetDataBaseList.CommandType = CommandType.Text;
+                cmdGetDataBaseList.Connection = cnn;
+                reader = cmdGetDataBaseList.ExecuteReader();
+                tblMyDatabaseList.Load(reader);
+                foreach (DataRow row in tblMyDatabaseList.Rows)
+                {
+                    chklsbxDBList.Items.Add(row["name"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! " + Environment.NewLine + ex.ToString());
+            }
+            cnn.Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connectDatabase();
+            }
+            catch (Exception ex)
+            {
+                rchtxtLogs.Text = ex.ToString();
+            }
+
+        }
+        private void queryExecute(string datanbaseName, string query)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=" + txtServerName.Text.ToString() + ";Initial Catalog=" + datanbaseName + ";User ID=" + txtLogin.Text.ToString() + ";Password=" + txtPassword.Text.ToString();
+            cnn = new SqlConnection(connetionString);
+            SqlCommand cmdGetDataBaseList = new SqlCommand();
+            DataTable tblMyDatabaseList = new DataTable();
+            try
+            {
+                cnn.Open();
+                cmdGetDataBaseList.CommandText = query;
+                cmdGetDataBaseList.CommandType = CommandType.Text;
+                cmdGetDataBaseList.Connection = cnn;
+                cmdGetDataBaseList.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+            cnn.Close();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < chklsbxDBList.Items.Count; i++)
+            {
+
+                if (chklsbxDBList.GetItemChecked(i))
+                {
+                    try
+                    {
+                        queryExecute(chklsbxDBList.Items[i].ToString(), rchtxtLogs.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+            MessageBox.Show("Your query've run for all Database that you checked");
+        }
+        private string getQueryString()
+        {
+            string executeQuery;
+            OpenFileDialog requestFileDialog = new OpenFileDialog();
+            DialogResult result = requestFileDialog.ShowDialog(); // Show the dialog.
+            executeQuery = requestFileDialog.FileName;
+            return executeQuery;
+        }
+
+        private void btnImportQuery_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rchtxtLogs.Text = File.ReadAllText(getQueryString());
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Application will be closed !!!!!", "Exit confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+                this.Dispose();
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                this.Show();
+            }
+        }
+
+        private void chklsbxDBList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                for (int i = 0; i < chklsbxDBList.Items.Count; i++)
+                {
+                    if (!chklsbxDBList.GetItemChecked(i))
+                    {
+                        chklsbxDBList.SetItemChecked(i, true);
+                    }
+                }
+            }
+        }
+
+        private void chklsbxDBList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool dbcheckState = chklsbxDBList.GetItemCheckState(chklsbxDBList.SelectedIndex) == CheckState.Checked ? true : false;
+            chklsbxDBList.SetItemCheckState(chklsbxDBList.SelectedIndex, dbcheckState == true ? CheckState.Checked : CheckState.Unchecked);
+        }
+
+        private void chklsbxDBList_KeyUp(object sender, KeyEventArgs e)
+        {
+            bool dbcheckState = chklsbxDBList.GetItemCheckState(chklsbxDBList.SelectedIndex) == CheckState.Checked ? true : false;
+            chklsbxDBList.SetItemCheckState(chklsbxDBList.SelectedIndex, dbcheckState == true ? CheckState.Checked : CheckState.Unchecked);
+        }
+    }
+}
+
